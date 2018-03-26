8516ea8f7ff6eb87c18da45dcb47518142e88a07
baptiste drelon <baptiste200759@outlook.com>
2018-03-26 17:42:58 +0200

    avant de remplacer par JonnyB

--- app/src/main/java/com/example/baptiste/smack/Controllers/CreateUserActivity.kt
+++ app/src/main/java/com/example/baptiste/smack/Controllers/CreateUserActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.baptiste.smack.R
+import com.example.baptiste.smack.Services.AuthService
+import com.example.baptiste.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -52,6 +54,27 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun createUserClicked(view: View){
 
+        val userName = createUserNameText.text.toString()
+        val email = createEmailText.text.toString()
+        val password =  createPasswordText.text.toString()
+
+        AuthService.registerUser(this, email, password){registerSuccess ->
+            if(registerSuccess){
+                AuthService.loginUser(this, email,password){loginSuccess ->
+                    if(loginSuccess){
+                     AuthService.createUser(this,userName,email, userAvatar, avatarColor){createSuccess ->
+                         if(createSuccess){
+                             println(UserDataService.name)
+                             println(UserDataService.avatarColor)
+                             println(UserDataService.avatarName)
+                             finish()
+                         }
+                     }
+
+                    }
+                }
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/baptiste/smack/Services/UserDataService.kt
@@ -0,0 +1,14 @@
+package com.example.baptiste.smack.Services
+
+/**
+ * Created by Baptiste on 26/03/2018.
+ */
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+}
